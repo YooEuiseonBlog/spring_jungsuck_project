ea406adc93e723d95ebf1056afcfc32109dd89bb
esy <yesman5307@gmail.com>
2022-08-30 04:15:55 +0900

    commit project

---
+++ src/main/java/com/fastcampus/ch4/controller/SimpleRestController.java
@@ -0,0 +1,28 @@
+package com.fastcampus.ch4.controller;
+
+import com.fastcampus.ch4.domain.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+//@RestController
+@Controller
+public class SimpleRestController {
+//    @GetMapping("/ajax")
+//    public String ajax() {
+//        return "ajax";    // 뷰이름
+//    }
+    @GetMapping("/test")
+        public String test() {
+            return "test";  // 뷰이름
+        }
+
+    @PostMapping("/send")
+    @ResponseBody
+    public Person test(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
+    }
+}
